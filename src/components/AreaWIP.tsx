29f67d90039778b6cbb663fc43b3e8fed2a32e0b
wdwf <w_dwf@hotmail.com>
2023-12-01 12:45:41 -0300

    feat: add styled of areaWIP

---
+++ src/components/AreaWIP.tsx
@@ -0,0 +1,9 @@
+import fillet from '../assets/fillet.png';
+
+export function AreaWIP() {
+  return (
+    <div className="w-full h-full absolute overflow-hidden z-10">
+      <img src={fillet} alt="" className="scale-150 py-60" />
+    </div>
+  );
+}
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,25 +1,30 @@
-import { CardProjects } from '../components/cardProject';
+import { CardProjects } from '../components/CardProject';
 import ThumbnailProjectOne from '../assets/ThumbnailProjectOne.png';
+import { AreaWIP } from '../components/areaWIP';
 
 export function Projects() {
   return (
-    <section>
-      <div>
-        <div>
-          <h3>Projetos</h3>
-          <h3>Desenvolvidos</h3>
+    <section id="projects" className="w-full my-16 relative">
+      <AreaWIP />
+      <div className="blur-md">
+        <div className="flex items-center pb-8 px-8 justify-between">
+          <div className="text-3xl font-Modelica02B">
+            <h3>Projetos</h3>
+            <h3>Desenvolvidos</h3>
+          </div>
+          <p className="font-Satoshi text-gray-400 leading-8 w-[42%]">
+            Embarcando em uma jornada de aprendizado, posso aos poucos mostrar os frutos desses estudos, onde o código
+            se entrelaça com a experiência do usuário.
+          </p>
         </div>
-        <p>
-          Embarcando em uma jornada de aprendizado, posso aos poucos mostrar os frutos desses estudos, onde o código se
-          entrelaça com a experiência do usuário.
-        </p>
-      </div>
-      <div>
-        <CardProjects
-          title="Name Project"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae facilisis nisi, non fringilla ex. Aliquam dictum eleifend."
-          image={ThumbnailProjectOne}
-        />
+        <div className="flex gap-8">
+          <CardProjects
+            title="Name Project"
+            description="Revolucione o aprendizado de inglês com nossa plataforma de videochamadas dedicada a aulas personalizadas. Conecte-se com instrutores qualificados para uma experiência de ensino imersiva e eficaz."
+            image={ThumbnailProjectOne}
+            frameworks={['React', 'typescript', 'postgresql']}
+          />
+        </div>
       </div>
     </section>
   );
