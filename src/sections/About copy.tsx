758af08c3ac264b523082afca33f52826732e0e8
wdwf <w_dwf@hotmail.com>
2023-12-19 23:37:38 -0300

    feat: add about responsive

--- src/sections/About copy.tsx
+++ src/sections/About copy.tsx
@@ -5,19 +5,19 @@ import photo from '../assets/photo.png';
 
 export function About() {
   return (
-    <section id="about" className="w-full min-h-screen mt-3">
-      <div className="bg-zinc-300 text-black flex items-center py-6 pl-5 gap-9 justify-between">
+    <section id="about" className="min-h-screen mt-3 //bg-slate-600">
+      <div className="bg-zinc-300 text-black flex items-center px-16 h-40 gap-9 justify-between">
         <div>
           <p className="text-3xl font-black font-Modelica02B">Inicio</p>
           <p className="text-xl font-black font-Modelica02M">Agosto, Terça-feira 1, 2017</p>
         </div>
-        <div className="hidden lg:flex border-x-2 border-gray-600 px-8 h-16 w-2/4 items-center">
+        <div className="border-x-2 border-gray-600 px-8 h-4/5 w-2/4 flex items-center">
           <hr className="border-[1.5px] w-full border-gray-600" />
         </div>
-        <img src={shape} alt="forma simples" className="h-3/5 mr-7 hidden lg:block" />
+        <img src={shape} alt="forma simples" className="h-3/5 mr-7" />
       </div>
-      <div className="w-full flex flex-col lg:flex-row">
-        <div className="font-Satoshi text-gray-400 py-5 px-5 leading-8">
+      <div className="flex">
+        <div className="font-Satoshi text-gray-400 pt-10 pl-16 pr-52 leading-8">
           <p className="mb-8">
             Iniciei minha jornada como desenvolvedor Front-End em 2017 por meio do curso de desenhista de páginas para
             web disponibilizado pelo SENAI. Desde então, sigo praticando e desenvolvendo minhas hard skills (Logica de
@@ -34,7 +34,7 @@ export function About() {
           <div className="bg-indigo-600 flex items-center justify-center h-12 py-2">
             <img src={arrowGroup} alt="" className="h-full" />
           </div>
-          <img src={photo} alt="" className="m-auto" />
+          <img src={photo} alt="" />
         </div>
       </div>
       <div className="bg-zinc-300 gap-8 flex items-center min-h-[50px] h-14 py-2 px-8">
